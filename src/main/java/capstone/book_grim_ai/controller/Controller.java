deaa907a356b51e9f79ed8e7a085b9969d97e3cb
99kenny <helloiamkenny@gmail.com>
2023-05-13 02:17:30 +0900

    fix: cmd command

--- src/main/java/capstone/book_grim_ai/controller/Controller.java
+++ src/main/java/capstone/book_grim_ai/controller/Controller.java
@@ -25,11 +25,11 @@ public class Controller {
     public ResponseEntity<byte[]> createCharacter(@RequestPart(value = "prompt") String prompt,
                                                   @RequestPart(value = "image") MultipartFile img) throws IOException {
         // ControlNet 돌리기
-        File file = new File("./origin_img/" + img.getOriginalFilename());
+        File file = new File("./resources/origin_img/" + img.getOriginalFilename());
         img.transferTo(file);
-        Runtime.getRuntime().exec("cmd / " + "python3.10 book_grim.py -img " + file.getPath() + " -p \""+ prompt +"\"");
+        Runtime.getRuntime().exec("cmd / " + "sudo python3.10 book_grim.py -img " + file.getPath() + " -p \""+ prompt +"\"");
 
-        InputStream imageStream = new FileInputStream("./ControlNet-with-anything-v4/results/output.png");
+        InputStream imageStream = new FileInputStream("~/ControlNet-with-anything-v4/results/output.png");
         byte[] imageByteArray = IOUtils.toByteArray(imageStream);
         imageStream.close();
         return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
